68fdb3ad6dc04264de33433d867a7297a0b8798b
Angel <abasurco0@gmail.com>
2025-03-03 12:00:24 +0100

    actualizacion de busqueda de menu

--- app/src/main/java/com/angel/horoscopo/MainActivity.kt
+++ app/src/main/java/com/angel/horoscopo/MainActivity.kt
@@ -12,20 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    val horoscopeList = listOf(
-        Horoscope("aries", R.string.horoscope_name_aries, R.string.horoscope_date_aries, R.drawable.aries_icon),
-        Horoscope("taurus", R.string.horoscope_name_taurus, R.string.horoscope_date_taurus, R.drawable.taurus_icon),
-        Horoscope("gemini", R.string.horoscope_name_gemini, R.string.horoscope_date_gemini, R.drawable.gemini_icon),
-        Horoscope("cancer", R.string.horoscope_name_cancer, R.string.horoscope_date_cancer, R.drawable.cancer_icon),
-        Horoscope("leo", R.string.horoscope_name_leo, R.string.horoscope_date_leo, R.drawable.leo_icon),
-        Horoscope("virgo", R.string.horoscope_name_virgo, R.string.horoscope_date_virgo, R.drawable.virgo_icon),
-        Horoscope("libra", R.string.horoscope_name_libra, R.string.horoscope_date_libra, R.drawable.libra_icon),
-        Horoscope("scorpio", R.string.horoscope_name_scorpio, R.string.horoscope_date_scorpio, R.drawable.scorpio_icon),
-        Horoscope("sagittarius", R.string.horoscope_name_sagittarius, R.string.horoscope_date_sagittarius, R.drawable.sagittarius_icon),
-        Horoscope("capricorn", R.string.horoscope_name_capricorn, R.string.horoscope_date_capricorn, R.drawable.capricorn_icon),
-        Horoscope("aquarius", R.string.horoscope_name_aquarius, R.string.horoscope_date_aquarius, R.drawable.aquarius_icon),
-        Horoscope("pisces", R.string.horoscope_name_pisces, R.string.horoscope_date_pisces, R.drawable.pisces_icon)
-    )
+    var horoscopeList: List<Horoscope> = Horoscope.horoscopeList
 
     lateinit var recycleView : RecyclerView
 
@@ -45,8 +32,9 @@ class MainActivity : AppCompatActivity() {
 
         val adapter =  HoroscopeAdapter (horoscopeList) { position ->
         val horoscope = horoscopeList[position]
+
         val intent = Intent(this, DetailActivity::class.java)
-        intent.putExtra("horoscope_ID_EXTRA",horoscope.id)
+        intent.putExtra("horoscope_id",horoscope.id)
         startActivity(intent)
         }
 
--- app/src/main/java/com/angel/horoscopo/horoscopeActivity.kt
+++ app/src/main/java/com/angel/horoscopo/horoscopeActivity.kt
@@ -5,7 +5,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.motion.widget.MotionScene.Transition.TransitionOnClick
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
